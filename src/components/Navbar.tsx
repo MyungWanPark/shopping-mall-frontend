db0f1b11a0845cec65e2083fac8efb34695c1b32
parkmyungwan <mywanpark@gmail.com>
2023-02-06 18:15:07 +0900

    style: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,15 +5,15 @@ import { AiOutlineEdit } from 'react-icons/ai';
 
 export default function Navbar() {
     return (
-        <header>
-            <Link to={'/'}>
+        <header className="flex justify-between border-b border-gray-300 p-2">
+            <Link to={'/'} className="flex items-center text-3xl text-brand">
                 <BiShoppingBag />
                 <h1>Shopping Mall</h1>
             </Link>
-            <nav>
+            <nav className="flex items-center gap-4 font-semibold">
                 <Link to={'/products'}>Products</Link>
                 <Link to={'/carts'}>Carts</Link>
-                <Link to={'/products/new'}>
+                <Link to={'/products/new'} className="text-2xl">
                     <AiOutlineEdit />
                 </Link>
                 <button>Login</button>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                brand: '#f96162',
+            },
+        },
+    },
+    plugins: [],
+};
