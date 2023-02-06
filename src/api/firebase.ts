3c79f716e498af52d00e2533cba5282096b67cad
parkmyungwan <mywanpark@gmail.com>
2023-02-06 22:06:19 +0900

    feat: login & logout using firebase

---
+++ src/api/firebase.ts
@@ -0,0 +1,58 @@
+import { initializeApp } from 'firebase/app';
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    signOut,
+    onAuthStateChanged,
+    User,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+export async function login() {
+    return signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            // const credential = GoogleAuthProvider.credentialFromResult(result);
+            // const token = credential?.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            console.log(user);
+            return user;
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            // const errorCode = error.code;
+            // const errorMessage = error.message;
+            // The email of the user's account used.
+            // const email = error.customData.email;
+            // The AuthCredential type that was used.
+            // const credential = GoogleAuthProvider.credentialFromError(error);
+            console.log(error);
+        });
+}
+
+export async function logout() {
+    return signOut(auth)
+        .then(() => {
+            // Sign-out successful.
+            return null;
+        })
+        .catch((error) => {
+            // An error happened.
+        });
+}
+
+export function onUserStateChanged(callback: (user: User) => void) {
+    onAuthStateChanged(auth, (user) => callback(user!));
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BiShoppingBag } from 'react-icons/bi';
 import { AiOutlineEdit } from 'react-icons/ai';
+import { login, logout, onUserStateChanged } from './../api/firebase';
+import { User } from 'firebase/auth';
 
 export default function Navbar() {
+    const [user, setUser] = useState<User>();
+    useEffect(() => {
+        onUserStateChanged((user) => setUser(user));
+    }, []);
+
+    const handleLogin = () => {
+        login().then((user) => {
+            user && setUser(user);
+        });
+    };
+    const handleLogout = () => {
+        logout().then(() => setUser(undefined));
+    };
     return (
         <header className="flex justify-between border-b border-gray-300 p-2">
             <Link to={'/'} className="flex items-center text-3xl text-brand">
@@ -16,7 +31,8 @@ export default function Navbar() {
                 <Link to={'/products/new'} className="text-2xl">
                     <AiOutlineEdit />
                 </Link>
-                <button>Login</button>
+                {!user && <button onClick={handleLogin}>Login</button>}
+                {user && <button onClick={handleLogout}>Logout</button>}
             </nav>
         </header>
     );
