b1e1f08cb01aedc324d927c14255a9b5d1b2a2f9
MyungWan Park <mywanpark@gmail.com>
2025-03-06 23:31:53 +0900

    feat: use Skeleton until all imgs are uploaded for the better ux

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ProductType } from '../../types/product';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 type Props = {
     product: ProductType;
+    setLoadedImg: () => void;
+    showSkeleton: boolean;
 };
-export default function ProductCard({ product: { name, price, category, imgURL, id }, product }: Props) {
+export default function ProductCard({
+    product: { name, price, category, imgURL, id },
+    showSkeleton,
+    setLoadedImg,
+}: Props) {
     const navigate = useNavigate();
     const handleClick = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
         navigate(`/products/${id}`);
     };
     return (
         <li
-            className="cursor-pointer shadow-md rounded-lg font-Raleway overflow-hidden transition-all hover:scale-105"
+            className="relative cursor-pointer h-80 shadow-md rounded-lg font-Raleway overflow-hidden duration-300 transition-all hover:scale-105"
             onClick={handleClick}
         >
-            <img src={imgURL} alt={name} className="h-112 tablet:h-128 w-full lg:h-104" />
+            {showSkeleton && (
+                <div className="absolute inset-0">
+                    <Skeleton className="rounded-2xl h-80" height="" />
+                </div>
+            )}
+            <img src={imgURL} alt={name} className="h-[80%] w-full" loading="lazy" onLoad={setLoadedImg} />
             <div className="flex justify-between items-center text-xs sm:text-md p-2 lg:text-lg">
-                <h3 className="truncate">{name}</h3>
-                <p>₩{price!.toLocaleString()} 원</p>
+                <h3 className="truncate w-[60%]">{name}</h3>
+                <p className="text-sm">₩{price!.toLocaleString()} 원</p>
             </div>
-            <p className="text-gray-300 px-2 pb-2 text-xs sm:text-md">{category}</p>
+            <p className="text-gray-400 px-2 pb-2 text-xs sm:text-md text-center">{category}</p>
         </li>
     );
 }
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from './ProductCard';
 import useProducts from '../../hooks/useProducts';
 import { useSearchParams } from 'react-router-dom';
@@ -15,6 +15,7 @@ export default function Products({ showAllProduct }: Prop) {
     let category = searchParams.get('category') as Category;
     const keyword = searchParams.get('keyword') as string | undefined;
     const page = parseInt(searchParams.get('page') || '1', 10);
+    const [loadedImg, setLoadedImg] = useState(0);
 
     if (showAllProduct) {
         category = 'all';
@@ -29,15 +30,23 @@ export default function Products({ showAllProduct }: Prop) {
     const { totalPages, products, count } = data as unknown as PaginationData;
     const PaginationButtonArr = getPaginaionButtons(page, totalPages);
 
+    const totalImgs = products.length;
+    const isAllImgLoaded = totalImgs === loadedImg;
+
     return (
         <>
             <p className="px-4 pt-2 text-sm lg:text-lg">{count} products found.</p>
             {isLoading && <p>isLoading..</p>}
             {error && <p>{error}</p>}
             {Array.isArray(products) && (
-                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4">
                     {products.map((product) => (
-                        <ProductCard key={product.id} product={product} />
+                        <ProductCard
+                            key={product.id}
+                            product={product}
+                            showSkeleton={!isAllImgLoaded}
+                            setLoadedImg={() => setLoadedImg((prev) => prev + 1)}
+                        />
                     ))}
                 </ul>
             )}
